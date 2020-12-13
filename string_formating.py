baf7799c5079f96cb07dec5eeb3d3e9e6f532e9a
lisanerd <lisa.rm.nyc@gmail.com>
2020-12-13 14:26:46 -0400

    I printed "Hello" 10 times and "World" 3 times using x and y variables.

--- string_formating.py
+++ string_formating.py
@@ -1,3 +1,10 @@
 # testing string formating
 
 print("hello. welcome to lisanerd's repo!!")
+
+x = "Hello"
+y = "World"
+x_list = [x] * 10
+y_list = [y] * 3
+all_list = x_list + y_list
+print(all_list)
