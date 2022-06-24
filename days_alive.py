3004d26199c77aeea5804b948bec7f1839380481
lisanerd <lisa.rm.nyc@gmail.com>
2022-06-23 22:53:20 -0400

    Cleaned up code and removed unnecessary function/variable. Added new part to the program that tells you the date when you will be 10000 days old.

--- days_alive.py
+++ days_alive.py
@@ -1,17 +1,18 @@
 
-from datetime import date
+from datetime import date, timedelta
+import datetime
 
 answer_year =int(input("What year were you born in? "))
 answer_month = int(input("What month were you born in? "))
 answer_day = int(input("What day were you born on? "))
 
-def num_of_days(answer:int, date_2):
-    return(date_2 - answer)
-
 today = date.today()
 
-date_1 = date(answer_year, answer_month, answer_day)
-date_2 = today
-dif = num_of_days(date_1, date_2)
+birthdate = date(answer_year, answer_month, answer_day)
+dif = today - birthdate
 
 print(f"You were born {dif.days} days ago")
+
+end_date = birthdate + datetime.timedelta(days = 10000)
+
+print(f"You will be 10000 days old on {end_date}")
