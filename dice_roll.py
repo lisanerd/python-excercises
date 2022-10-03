fa8bcc1657da5c955e87a21f003504efcfe05915
lisanerd <lisa.rm.nyc@gmail.com>
2022-10-03 19:57:10 -0400

    Dice is printed and shows dice value of dice thrown in that round.

--- dice_roll.py
+++ dice_roll.py
@@ -2,8 +2,12 @@
 import random
 
 
-
 class SmartDice:
+    DICE_N = """         _______
+        |       |
+        |  {:2d}   |
+        |_______|"""
+
     def __init__(self, sides=6) -> None:
         self.sides = sides
         self.sum_of_all_throw_ever = 0
@@ -16,31 +20,20 @@ class SmartDice:
         return number
 
     def draw(self):
-        pass
+        print(self.DICE_N.format(self.all_throws[-1]))
+    
+    def draw_all_throws(self):
+        for a in self.all_throws:
+            self.draw(s)
 
-
-DICE_N = """     _______
-    |       |
-    |   {}   |
-    |_______|"""
-
-def throw_dice(number_of_dice:int = 1, dice_sides_number = 6):
-    print('-'*8)
-    total = 0
-    for d in range(number_of_dice):
-        number = random.randint(1, dice_sides_number)
-        face = DICE_N.format(number)
-        print(face)
-        total += number
-    return total
 
 #########################################
 
 # tot = throw_dice()
 # tot = throw_dice(number_of_dice=2, dice_sides_number=24)
 
-dice_1 = SmartDice(sides=8)
-dice_2 = SmartDice(sides=32)
+dice_1 = SmartDice()
+dice_2 = SmartDice()
 
 dice_1.throw()
 dice_1.throw()
@@ -48,11 +41,13 @@ dice_1.throw()
 dice_1.throw()
 dice_2.throw()
 
-tot = dice_1.sum_of_all_throw_ever + dice_2.sum_of_all_throw_ever
+tot = dice_1.sum_of_all_throw_ever
 
 print(dice_1.all_throws, dice_2.all_throws)
 # throw_dice()
 # throw_dice(3)
 
 
+dice_1.draw()
+dice_2.draw()
 print(f"Your total dice sum is {tot}")
