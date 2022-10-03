dc11d26eb38872f7a5dafac3cc90c83fffc64f07
lisanerd <lisa.rm.nyc@gmail.com>
2022-10-03 19:29:28 -0400

    Added a "Class" to neaten up my code (and my function).

--- dice_roll.py
+++ dice_roll.py
@@ -3,18 +3,32 @@ import random
 
 
 
-DICE_SIDES_NUMBER = 6
+class SmartDice:
+    def __init__(self, sides=6) -> None:
+        self.sides = sides
+        self.sum_of_all_throw_ever = 0
+        self.all_throws = []
+
+    def throw(self):
+        number = random.randint(1, self.sides)
+        self.all_throws.append(number)
+        self.sum_of_all_throw_ever += number
+        return number
+
+    def draw(self):
+        pass
+
 
 DICE_N = """     _______
     |       |
     |   {}   |
     |_______|"""
 
-def throw_dice(number_of_dice:int = 1):
+def throw_dice(number_of_dice:int = 1, dice_sides_number = 6):
     print('-'*8)
     total = 0
     for d in range(number_of_dice):
-        number = random.randint(1, DICE_SIDES_NUMBER)
+        number = random.randint(1, dice_sides_number)
         face = DICE_N.format(number)
         print(face)
         total += number
@@ -22,7 +36,21 @@ def throw_dice(number_of_dice:int = 1):
 
 #########################################
 
-tot = throw_dice(number_of_dice=2)
+# tot = throw_dice()
+# tot = throw_dice(number_of_dice=2, dice_sides_number=24)
+
+dice_1 = SmartDice(sides=8)
+dice_2 = SmartDice(sides=32)
+
+dice_1.throw()
+dice_1.throw()
+dice_1.throw()
+dice_1.throw()
+dice_2.throw()
+
+tot = dice_1.sum_of_all_throw_ever + dice_2.sum_of_all_throw_ever
+
+print(dice_1.all_throws, dice_2.all_throws)
 # throw_dice()
 # throw_dice(3)
 
