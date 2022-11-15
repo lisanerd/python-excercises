03ec2a2797dfbefa95a35f43604c8a753b3af249
lisanerd <lisa.rm.nyc@gmail.com>
2022-11-15 18:01:36 -0500

    Improvement on the dice program.

--- dice_roll.py
+++ dice_roll.py
@@ -1,4 +1,5 @@
 
+from operator import truediv
 import random
 
 
@@ -51,3 +52,52 @@ print(dice_1.all_throws, dice_2.all_throws)
 dice_1.draw()
 dice_2.draw()
 print(f"Your total dice sum is {tot}")
+
+
+class Game:
+    def __init__(self, goal) -> None:
+        self.score_player_1 = 0
+        self.score_player_2 = 0
+        self.dice = SmartDice(sides=6)
+        self.goal=goal
+    
+    def finished(self):
+        if self.score_player_1 > self.goal or self.score_player_2 > self.goal:
+            return True
+        return False
+    
+    def play_turn(self):
+        v1 = self.dice.throw()
+        self.score_player_1 += v1
+        v2 = self.dice.throw()
+        self.score_player_2 += v2
+
+    def announce(self):
+        if self.score_player_1 > self.score_player_2:
+            print("The winner is player 1!")
+        elif self.score_player_2 > self.score_player_1:
+            print("The winner is player 2!")
+        else:
+            print("It's a draw!")
+
+my_game = Game(goal=20)
+while not my_game.finished():
+    my_game.play_turn()
+my_game.announce()
+
+sec_game = Game(40)
+while not sec_game.finished():
+    sec_game.play_turn()
+sec_game.announce()
+
+
+class GameSpecial(Game):
+    def play_turn(self):
+        super().play_turn()
+        self.score_player_1 += 1
+
+
+brand_new_game = GameSpecial(40)
+while not brand_new_game.finished():
+    brand_new_game.play_turn()
+brand_new_game.announce()
